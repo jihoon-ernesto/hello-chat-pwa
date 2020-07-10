1a2ce5f7b803bdca148373aca681121e1fa0d86e
jihoon <jihoon@hancom.com>
2020-07-10 13:33:31 +0900

    Add 3D models: using vue-3d-model lib
    - ref) https://github.com/hujiulong/vue-3d-model

---
+++ src/components/Avatar.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="container">
+
+  <div class="model-box">
+    <model-gltf
+      clsss="model-3d"
+      src="https://hujiulong.github.io/vue-3d-model/static/models/gltf/Duck/glTF/Duck.gltf">
+    </model-gltf>
+  </div>
+
+  <div class="model-box">
+    <model-fbx
+      clsss="model-3d"
+      src="https://hujiulong.github.io/vue-3d-model/static/models/fbx/dancing.fbx">
+    </model-fbx>
+  </div>
+
+</div>
+</template>
+
+<script>
+import {
+  ModelGltf,
+  ModelFbx,
+} from 'vue-3d-model';
+
+export default {
+  name: 'avatar',
+  components: {
+    ModelGltf,
+    ModelFbx,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 100%;
+
+  .model-box {
+    width: 100%;
+    height: 200px;
+  }
+}
+</style>
